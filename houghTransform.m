b78f2802121cc50577c00cba4f6e51d711a5b585
Niyaz94 <niyaz94nz@gmail.com>
2020-12-01 20:01:39 +0300

    adding report 3

--- houghTransform.m
+++ houghTransform.m
@@ -1,26 +1,27 @@
 %% Creating a simple image
 clear
+% create 101 by 101 binar image all pixels are zero just the corners and image center are one
 im = zeros(101,101);
 im(1,1) = 1;
 im(1,end) = 1;
 im(end,1) = 1;
 im(end,end) = 1;
 im(floor(end/2),floor(end/2)) = 1;
-%% Initializing other parameters
+
+%% define theta range it start from -90 to 90
 theta = ((-90:90)./180).*pi;
 
-%length of the diagonal of image
-%DiagLen = hypot(size(im,1),size(im,2))% Invoke Euclid To Get The Diagonal Length
 
+% find Diagonal length of image. you can use matlab functions[hypot(size(im,1),size(im,2))]
 D = sqrt(size(im,1).^2 + size(im,2).^2);
-
+% create hough Space which his row equal [2*Diagonal] and his coloumn equal [theta length (180 in our case)]
 HS = zeros(ceil(2.*D),numel(theta));
-
-% find the index of non zero pixels
+% find the index of non zero pixels and puting them inside x, y array
 [y,x] = find(im);
 % subtract all the items in array by one
 y = y - 1;
 x = x - 1;
+
 % numel(x) ---> get the lenght of array
 %A cell array is a data type with indexed data containers called cells, where each cell can contain any type of data. Cell arrays commonly contain either lists of character vectors of different lengths, or mixes of strings and numbers, or numeric arrays of different sizes.
 rho = cell(1,numel(x));
@@ -29,15 +30,14 @@ rho = cell(1,numel(x));
 
 %% Calculating the Hough Transform
 for i = 1: numel(x)
-    rho{i} = x(i).*cos(theta) + y(i).*sin(theta); % [-sqrt(2),sqrt(2)]*D rho interval
+    % Calculate rho, D is added for a positive index
+    rho{i} = floor((x(i).*cos(theta) + y(i).*sin(theta))+ D) + 1;
 end
 
 %% Creating the Hough Space as an Image
 for i = 1:numel(x)
-    %converting minus value to posative becuase we don't have row with
-    %minus index in image
-    rho{i} = floor(rho{i}+ D) + 1;
     for j = 1:numel(rho{i})
+        % Vote in the hough accumulator
         HS(rho{i}(j),j) = HS(rho{i}(j),j) + 1; 
     end
 end
